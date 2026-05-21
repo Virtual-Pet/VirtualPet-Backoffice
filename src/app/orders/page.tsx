98a7938ac587c142de31ae3441e24f521239c82f
Juan <juancsaladino@gmail.com>
2026-05-21 16:29:15 -0300

    fix(lint): resolve github actions linting errors

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,4 +1,4 @@
-﻿"use client";
+"use client";
 
 import { useEffect, useState, useCallback } from "react";
 import { formatPrice } from "@/lib/api";
@@ -49,6 +49,7 @@ export default function ManagerOrdersPage() {
   }, []);
 
   useEffect(() => {
+    // eslint-disable-next-line
     loadOrders(activeTab);
   }, [activeTab, loadOrders]);
 
